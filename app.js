f52bcafe205837de7943c4b0bfb5b2283e765b80
Raajes Kumar K S <raajeskumar.yoho@gmail.com>
2025-09-10 14:31:51 +0530

    first commit

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
+    "http://mgmproperties.co.in",
 ];
 app.use(
     cors({
